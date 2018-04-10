7c04629181678615750a63f5d9bd42a29f85c8ac
necro-wbj <d04220837@gm.tut.edu.tw>
2018-04-10 16:16:39 +0800

    修正迴圈數值為更加人性化

--- AND.py
+++ AND.py
@@ -1,11 +1,12 @@
 import random
 h = [0] * 3
 weight = [[random.triangular(-5, 5), random.triangular(-5, 5), 1]
-          for _ in range(2)]
+          for _ in range(len(h))]
 output = [0]
-weight2 = [[random.triangular(-5, 5)for _ in range(2)]for _ in range(1)]
-baise = [[random.random() for _ in range(2)] for _ in range(2)]
+weight2 = [[random.triangular(-5, 5)for _ in range(len(h))]
+           for _ in range(len(output))]
 temp = [0]
+train = [[0, 0], [0, 1], [1, 0], [1, 1]]
 # with open("xor_weight.txt", "r") as f:
 #     for line in f:
 #         for i in range(2):
@@ -19,23 +20,20 @@ def act(arr):
 
 
 def oact(arr):
-    for i in range(len(arr)):
-        if arr[i] >= 0.5:
-            return 1
-        else:
-            return 0
+    return(round(arr))
 
 
 def ai2(a):
-    global temp, h, weight, output, weight2, baise
+    global temp, h, weight, output, weight2
 
-    for j in range(2):
-        for i in range(2):
+    for j in range(len(h)):
+        for i in range(len(a)):
             h[j] += weight[j][i] * a[i]
+#            h=[h[j] + weight[j][a.index(i)] *i for i in a  for j in range(len (h))]
         h[j] += weight[j][2]
     act(h)
-    for j in range(1):
-        for i in range(1):
+    for j in range(len(output)):
+        for i in range(len(h)):
             output[j] += weight2[j][i] * h[i]
     act(output)
     # 正確答案
@@ -45,21 +43,21 @@ def ai2(a):
         temp[0] = 0
     # 倒傳遞:
     E = []
-    for j in range(1):
+    for j in range(len(output)):
         E.append((temp[j] - output[j]) * output[j] * (1 - output[j]))
-        for i in range(2):
+        for i in range(len(h)):
             weight2[j][i] = weight2[j][i] * 1 * E[j] * h[i]
     E2 = []
-    for j in range(2):
+    for j in range(len(h)):
         ei = 0
         for k in range(len(E)):
             ei += E[k] * weight2[k][j]
         E2.append(h[j] * (1 - h[j]) * ei)
-        for i in range(2):
-            weight[j][i] = weight[j][i] + 1 * E2[j] * a[i]
-        weight[j][i] = weight[j][2] + 1 * E2[j]
+        for i in range(len(a)):
+            weight[j][i] = weight[j][i] + 0.01 * E2[j] * a[i]
+        weight[j][i] = weight[j][2] + 0.01 * E2[j]
     print("輸出:", output[0])
-    print("輸出(轉換):", oact(output))
+    print("輸出(轉換):", oact(output[0]))
     print("正確", temp[0])
 
 
@@ -68,10 +66,11 @@ S = 0
 W = 1
 # while W > S:
 for i in range(1000):
-    a = [random.randint(0, 1), random.randint(0, 1)]
-    print("X=", a)
-    ai2(a)
-    print("")
+    for j in range(len(train)):
+        a = train[j]
+        print("X=", a)
+        ai2(a)
+        print("")
 # with open("xor_weight.txt", "w") as f:
 #     for s in range(2):
 #         for k in range(2):
