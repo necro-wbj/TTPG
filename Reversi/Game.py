ef5c82b0c74081bd83861ef9362af33724a832db
necro-wbj <d04220837@gm.tut.edu.tw>
2018-09-24 18:38:42 +0800

    完成自我訓練

--- Reversi/Game.py
+++ Reversi/Game.py
@@ -8,8 +8,11 @@ p2 = list()
 
 
 def start():
-    global chessboard
+    global chessboard, player, p1, p2
     chessboard = np.zeros((8, 8), dtype=float)
+    player = 1
+    p1 = list()
+    p2 = list()
     chessboard[3][3] = 1
     chessboard[3][4] = -1
     chessboard[4][3] = -1
@@ -47,52 +50,52 @@ def check(player):
     return canDown, player
 
 
-start()
-while end(chessboard):
-    input_data = list()
-    expect_data = list()
-    out = tr.out(chessboard).reshape((8, 8))
-    # print(np.argmax(tr.out(chessboard)))
-    # chessboard[chessboard == np.amax(out[check()])] = player
-    cnaDown, next_player = check(player)
-    out[cnaDown == False] = -1
-    # print(chessboard)
-    # print("-------------------------------------------------------------")
-    # print(out)
-    chessboard[out == np.amax(out)] = player
-    position = np.argmax(out.reshape((64,)))
-    scan.update(chessboard, position // 8, position % 8, player)
-    # print(out)
-    if player == 1:
-        p1.append([np.array(chessboard.reshape((64,))),
-                   np.array(out.reshape((64,)))])
-    else:
-        p2.append([np.array(chessboard.reshape((64,))),
-                   np.array(out.reshape((64,)))])
-    player = next_player
-for out in p1:
-    if np.size(chessboard[chessboard == 1]) > np.size(chessboard[chessboard == -1]):
-        out[1][out[1] == np.amax(out[1])] = 1
-        # print(out[1])
-        input_data.append(out[0])
-        expect_data.append(out[1])
-    else:
-        out[1][out[1] == np.amax(out[1])] = 0
-        # print(out[1].reshape((64,)))
-        input_data.append(out[0])
-        expect_data.append(out[1])
-for out in p2:
-    if np.size(chessboard[chessboard == 1]) < np.size(chessboard[chessboard == -1]):
-        out[1][out[1] == np.amax(out[1])] = 1
-        input_data.append(out[0].reshape((64,)))
-        expect_data.append(out[1].reshape((64,)))
-    else:
-        out[1][out[1] == np.amax(out[1])] = 0
-        input_data.append(out[0].reshape((64,)))
-        expect_data.append(out[1].reshape((64,)))
-batch = len(p1) + len(p2)
-
-tr.train(input_data, expect_data, batch)
+for _ in range(100000):
+    start()
+    while end(chessboard):
+        input_data = list()
+        expect_data = list()
+        out = tr.out(chessboard).reshape((8, 8))
+        # print(np.argmax(tr.out(chessboard)))
+        # chessboard[chessboard == np.amax(out[check()])] = player
+        cnaDown, next_player = check(player)
+        out[cnaDown == False] = -1
+        # print(chessboard)
+        # print("-------------------------------------------------------------")
+        # print(out)
+        chessboard[out == np.amax(out)] = player
+        position = np.argmax(out.reshape((64,)))
+        scan.update(chessboard, position // 8, position % 8, player)
+        # print(out)
+        if player == 1:
+            p1.append([np.array(chessboard.reshape((64,))),
+                       np.array(out.reshape((64,)))])
+        else:
+            p2.append([np.array(chessboard.reshape((64,))),
+                       np.array(out.reshape((64,)))])
+        player = next_player
+    for out in p1:
+        if np.size(chessboard[chessboard == 1]) > np.size(chessboard[chessboard == -1]):
+            out[1][out[1] == np.amax(out[1])] = 1
+            # print(out[1])
+            input_data.append(out[0])
+            expect_data.append(out[1])
+        else:
+            out[1][out[1] == np.amax(out[1])] = 0
+            # print(out[1].reshape((64,)))
+            input_data.append(out[0])
+            expect_data.append(out[1])
+    for out in p2:
+        if np.size(chessboard[chessboard == 1]) < np.size(chessboard[chessboard == -1]):
+            out[1][out[1] == np.amax(out[1])] = 1
+            input_data.append(out[0].reshape((64,)))
+            expect_data.append(out[1].reshape((64,)))
+        else:
+            out[1][out[1] == np.amax(out[1])] = 0
+            input_data.append(out[0].reshape((64,)))
+            expect_data.append(out[1].reshape((64,)))
+    batch = len(p1) + len(p2)
+    tr.train(input_data, expect_data, batch)
 
 # print("..................................................")
 # for i in p1:
